8b3dc84b745523b32061d96ffd7ce7945de629fc
Alex Gherghisan <alex.gherghisan@gmail.com>
2013-10-06 20:27:46 +0300

    Added MDN page suggestions when searching

--- mdnsearch.js
+++ mdnsearch.js
@@ -4,10 +4,45 @@ chrome.omnibox.setDefaultSuggestion({
 	description: 'Search Mozilla Developer Network for <match>%s</match>'
 });
 
-chrome.omnibox.onInputEntered.addListener(function(text) {
+var prevSearch = MDN_TOPICS || null;
+chrome.omnibox.onInputChanged.addListener(function(text, suggest) {
+	if (typeof prevSearch == 'undefined' || prevSearch === null) {
+		console.log('Topics not loaded');
+		return;
+	}
+
+	var regex = new RegExp('^' + text, 'i');
+	var keys = Object.keys(prevSearch).filter(function(topic) {
+		return regex.test(topic);
+	});
+
+	var search = keys.reduce(function(accumulator, key) {
+		accumulator[key] = prevSearch[key];
+		return accumulator;
+	}, {});
+
+	var suggestions =
+		Object.keys(search).map(function(key) {
+			return {
+				'content': search[key],
+				'description': key
+			};
+		}).sort(function(a, b) {
+			var ad = a['description'], bd = b['description'];
+			return ad > bd ? 1 : ad == bd ? 0 : -1;
+		});
+
+	suggest(suggestions);
+});
+
+var loadUrl = function(url) {
 	chrome.tabs.getSelected(null, function(tab) {
 		chrome.tabs.update(tab.id, {
-			url: MDN_SEARCH_URL + text
+			url: url
 		});
 	});
-})
+}
+
+chrome.omnibox.onInputEntered.addListener(function(text) {
+	loadUrl(/http/i.test(text) ? text : MDN_SEARCH_URL + text);
+});
